d8bae7075eca8294ea0c9e5feff1a0a3286ae290
DribDrob <drobushenko@gmail.com>
2022-05-02 22:39:52 +0300

    Commit Tasks 1-3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,39 +1,10 @@
-// import Player from '@vimeo/player';
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
-// Ознакомься с документацией библиотеки Vimeo плеера.
-// Добавь библиотеку как зависимость проекта через npm.
-// Инициализируй плеер в файле скрипта как это описано в секции pre-existing player, но учти что у тебя плеер добавлен как npm пакет, а не через CDN.
-// Разбери документацию метода on() и начни отслеживать событие timeupdate - обновление времени воспроизведения.
-// Сохраняй время воспроизведения в локальное хранилище. Пусть ключом для хранилища будет строка "videoplayer-current-time".
-// При перезагрузке страницы воспользуйся методом setCurrentTime() для того чтобы возобновить воспроизведение с сохраненной позиции.
-// Добавь в проект бибилотеку lodash.throttle и сделай так, чтобы время воспроизведения обновлялось в хранилище не чаще чем раз в секунду.
+const player = new Player('vimeo-player');
+const STORAGE_KEY = 'videoplayer-current-time';
 
-// const player = new Player('handstick', {
-//     id: 19231868,
-//     width: 640
-// });
+player.on('timeupdate', throttle((data)=> 
+localStorage.setItem(STORAGE_KEY, JSON.stringify(data.seconds)), 1000));
 
-// player.on('play', function() {
-//     console.log('played the video!');
-// });
-//////////////////////////////////////////////////////
-// on(event: string, callback: function): void
-// Add an event listener for the specified event. Will call the callback with a single parameter, data, that contains the data for that event. See events below for details.
-
-// const onPlay = function(data) {
-//     // data is an object containing properties specific to that event
-// };
-
-// player.on('play', onPlay);
-/////////////////////////////////////////////////////////
-// player.on('eventName', function(data) {
-//     // data is an object containing properties specific to that event
-// });
-///////////////////////////////////////////////
-// timeupdate Triggered as the currentTime of the video updates. It generally fires every 250ms, but it may vary depending on the browser.
-
-// {
-//     duration: 61.857
-//     percent: 0.049
-//     seconds: 3.034
-// }
+player.setCurrentTime(JSON.parse(localStorage.getItem(STORAGE_KEY)));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+import throttle from 'lodash.throttle';
+
+const STORAGE_KEY = 'feedback-form-state';
+
+const feedbackFormEl = document.querySelector('.feedback-form');
+const inputEl = document.querySelector('[name="email"]');
+const textareaEl = document.querySelector('[name="message"]');
+
+let formData = {
+    email: '',
+    message: '',
+};
+
+feedbackFormEl.addEventListener('input', throttle(onFormInputChange, 500));
+feedbackFormEl.addEventListener('submit', onFormSubmit);
+
+
+function onFormInputChange(e){
+formData[e.target.name] = e.target.value;
+localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function onFormSubmit (e) {
+e.preventDefault();
+console.log(formData);
+e.currentTarget.reset();
+localStorage.removeItem(STORAGE_KEY);
+}
+afterReloadPage();
+
+function afterReloadPage() {
+    const storageData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    if (storageData){
+    inputEl.value = storageData.email;
+    textareaEl.value = storageData.message;
+    formData.email = storageData.email;
+    formData.message = storageData.message;
+    };
+}
 
